828c964fb9559f140b642e20b4a9ea661ef82e5c
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-08-24 23:31:33 +0200

    meme index
    Co-Authored-By: Jens Birkbak Aagaard <9050040+jbaagaard@users.noreply.github.com>

--- Frontend/src/routes/meme/index.tsx
+++ Frontend/src/routes/meme/index.tsx
@@ -1,8 +1,9 @@
 import { Component, h } from "preact";
-import { Meme } from "../../models";
+import { Meme, MemeIdentification } from "../../models";
 import Loading from "../../components/Loading";
 import ky from "ky";
-import * as style from './style.css'
+import * as style from "./style.css";
+import { route } from "preact-router";
 
 interface Props {
     threadId: number;
@@ -13,12 +14,32 @@ interface State {
     meme?: Meme;
 }
 
+let muted = true;
+
 export default class MemePage extends Component<Props, State> {
     componentDidMount() {
-        ky.get(`/api/threads/${this.props.threadId}/${this.props.memeId}`)
+        this.loadMeme();
+    }
+
+    componentDidUpdate(previousProps: Readonly<Props>, previousState: Readonly<State>, snapshot: any) {
+        if(previousProps.memeId !== this.props.memeId){
+            this.loadMeme();
+        }
+    }
+
+    loadMeme = () => {
+        ky.get(`/api/memes/${this.props.threadId}/${this.props.memeId}`)
             .json<Meme>()
             .then(meme => this.setState({ meme }));
+    };
+
+    nextMeme = () => {
+        ky.get(`/api/memes`)
+            .json<MemeIdentification>()
+            .then(meme => route(`/${meme.threadId}/${meme.id}`));
     }
+
+    //<video src={`/api/memes/${this.props.threadId}/${this.props.memeId}/video`} />
 
     render({ memeId, threadId }: Props, { meme }: State) {
         if (!meme) return <Loading />;
@@ -29,12 +50,33 @@ export default class MemePage extends Component<Props, State> {
                     <div class={style.memeHeader}>
                         <div>{meme.name}</div>
                         <div class={style.headerControls}>
-                            <div class={`${style.nextButton} material-icons`}>navigate_next</div>
+                            <button
+                                class={`${style.nextButton} material-icons`}
+                                onClick={this.nextMeme}
+                            >
+                                navigate_next
+                            </button>
                         </div>
                     </div>
-                    <video src={`/api/${threadId}/${memeId}/video`} />
+                    <video
+                        key={meme.id}
+                        autoPlay
+                        muted={muted}
+                        controls
+                        onVolumeChange={(x:any) => {
+                             muted = x.target.muted;
+                        }}
+                    >
+                        <source
+                            src={`https://is2.4chan.org/wsg/${meme.fileId}.webm`}
+                            type={"video/webm"}
+                        />
+                        {/*<source src={`/api/memes/${this.props.threadId}/${this.props.memeId}/video`} type={"video/webm"}/>*/}
+                    </video>
                     <div class={style.memeFooter}>
-                        <button class={`${style.playButton} material-icons`}>play</button>
+                        <button class={`${style.playButton} material-icons`}>
+                            play
+                        </button>
                     </div>
                 </div>
             </div>
