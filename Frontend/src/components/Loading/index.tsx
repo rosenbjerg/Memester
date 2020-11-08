9992861b7c01d2247212912758a174ab3af43c11
Jens Birkbak Aagaard <teamtunfiske@gmail.com>
2020-11-08 17:45:34 +0100

    loading messages

--- Frontend/src/components/Loading/index.tsx
+++ Frontend/src/components/Loading/index.tsx
@@ -4,9 +4,10 @@ import * as style from "./style.css";
 const fullscreenStyle = { width: "100%", height: "100%" };
 const normalStyle = { display: "inline-block" };
 
-const  Loading = ({ fullscreen }: { fullscreen?: boolean }) => (
+const  Loading = ({ fullscreen, title }: { fullscreen?: boolean, title:string }) => (
     <span style={fullscreen ? fullscreenStyle : normalStyle}>
         <span class={style.loadingContainer}>
+            <div>{title}</div>
             <div class={style["sk-folding-cube"]}>
                 <div class={style["sk-cube"]}/>
                 <div class={`${style["sk-cube2"]} ${style["sk-cube"]}`}/>
