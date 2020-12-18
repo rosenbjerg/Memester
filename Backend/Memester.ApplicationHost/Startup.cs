96f895a21bbeb4ae690ea5ad1b97ca18b9ec3015
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-12-18 23:09:25 +0100

    More workers

--- Backend/Memester.ApplicationHost/Startup.cs
+++ Backend/Memester.ApplicationHost/Startup.cs
@@ -62,7 +62,7 @@ namespace Memester
             services.AddHangfire(ConfigureHangfire);
             services.AddHangfireServer(options =>
             {
-                options.WorkerCount = Math.Min(Environment.ProcessorCount * 3 - 1, 10);
+                options.WorkerCount = Math.Min(Environment.ProcessorCount * 4 - 1, 10);
                 options.Queues = JobQueues.All;
             });
             
